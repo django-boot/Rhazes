c944413ae2f3aeb962b881eb097204a12f2ac841
sepgh <sepehrgh01@gmail.com>
2023-10-06 13:43:41 +0330

    feat: added ModuleScanner

---
+++ tests/settings.py
@@ -0,0 +1,25 @@
+import os.path
+from pathlib import Path
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.sqlite3',
+        "NAME": ":memory:",
+    }
+}
+
+INSTALLED_APPS = (
+    "tests",
+)
+
+MIDDLEWARE = []
+
+USE_TZ = True
+TIME_ZONE = "UTC"
+SECRET_KEY = "foobar"
+
---
+++ tests/test_scanner.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+from rhazes.scanner import ModuleScanner
+
+
+class ModuleScannerTestCase(TestCase):
+
+    def test_scanner(self):
+        scanner = ModuleScanner(["tests.data.scanner"])
+        results = scanner.scan()
+        self.assertIn("tests.data.scanner.package", results)
+        self.assertIn("tests.data.scanner.package.module", results)
